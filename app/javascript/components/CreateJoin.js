eda687e097d2b814a2660ca08fca433306ebd90a
DF <dfwong5566@gmail.com>
2018-01-28 02:59:51 +0800

    ke yi post liao

--- app/javascript/components/CreateJoin.js
+++ app/javascript/components/CreateJoin.js
@@ -4,11 +4,25 @@ import RaisedButton from "material-ui/RaisedButton"
 
 class CreateJoin extends React.Component {
     clickHandler = () => {
-        fetch('/', {
+        fetch('localhost/', {
             method: 'post',
             body: {
             }
-        });
+        })
+            .then(
+                function(response) {
+                    if (response.status !== 200) {
+                        console.log('Looks like there was a problem. Status Code: ' +
+                                    response.status);
+                        return;
+                    }
+                    
+                    // Examine the text in the response
+                    response.json().then(function(data) {
+                        console.log(data);
+                    });
+                }
+            )
     }
     render () {
         return (

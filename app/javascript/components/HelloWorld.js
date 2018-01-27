f050c2027f2474bbd4d71d50a43358548e7f606f
DF <dfwong5566@gmail.com>
2018-01-28 02:41:11 +0800

    changed from const to class

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -6,64 +6,32 @@ import { BrowserRouter as Router,
        } from "react-router-dom"
 import RaisedButton from "material-ui/RaisedButton"
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import CreateJoin from "./CreateJoin.js"
 
 
-const HelloWorld = () => {
     const Home = () => (
         <div>
           <h2>Home</h2>
         </div>
     )
-    const About = () => (
-        <div>
-          <h2>About</h2>
-        </div>
-    )
+
+class HelloWorld extends React.Component{
+  render() {
     return (
         <MuiThemeProvider>
           <Router>
             <div>
               <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
+                <li><Link to="/createjoin">About</Link></li>
               </ul>
               <Route exact path="/" component={Home}/>
-              <Route path="/about" component={About}/>
+              <Route path="/createjoin" component={CreateJoin}/>
             </div>
           </Router>
         </MuiThemeProvider>
     )
+  }
 }
 
-//class HelloWorld extends React.Component {
-//    render () {
-//      return (
-//	<MuiThemeProvider>
-//          <Router>
-//            <div>
-//              <ul>
-//                <li><Link to="/">Home</Link></li>
-//                <li><Link to="/about">About</Link></li>
-//                <li><Link to="/topics">Topics</Link></li>
-//              </ul>
-//              
-//              <hr/>
-//              
-//              <Route exact path="/" component={Home}/>
-//              <Route path="/about" component={About}/>
-//              <Route path="/topics" component={Topics}/>
-//            </div>
-//          </Router>
-//      	  <div>
-//            <div>Greeting: {this.props.greeting}</div>
-//	    <RaisedButton> whjeee</RaisedButton>
-//      	  </div>
-//	</MuiThemeProvider>
-//    );
-//  }
-//}
-
-//HelloWorld.propTypes = {
-//  greeting: PropTypes.string
-//};
 export default HelloWorld
